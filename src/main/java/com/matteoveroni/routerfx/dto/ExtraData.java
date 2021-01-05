82b39dbacff293ed54b2e1a6690187381142ea2b
Matteo Veroni <matver87@gmail.com>
2021-01-05 18:06:08 +0100

    changed the structure of ExtraData

--- src/main/java/com/matteoveroni/routerfx/dto/ExtraData.java
+++ src/main/java/com/matteoveroni/routerfx/dto/ExtraData.java
@@ -1,13 +1,20 @@
 package com.matteoveroni.routerfx.dto;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.ToString;
 
 /**
  * @Author Matteo Veroni
  */
-@Data
+@Getter
+@ToString
 public class ExtraData {
 
     private final Object data;
     private final Class<?> dataClass;
+
+    public ExtraData(Object data) {
+        this.data = data;
+        this.dataClass = data.getClass();
+    }
 }

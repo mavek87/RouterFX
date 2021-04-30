2ed50a510c01bb3a0f9edb2483778af23b65d416
mavek <matver87@gmail.com>
2021-04-30 23:03:06 +0200

    commented module-info because it cause a crash in the build on jitpack.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,9 @@
-open module com.matteoveroni.routerfx {
-    requires javafx.base;
-    requires javafx.graphics;
-    requires javafx.fxml;
-    exports com.matteoveroni.routerfx.core;
-    exports com.matteoveroni.routerfx.dto;
-    exports com.matteoveroni.routerfx.factory;
-    exports com.matteoveroni.routerfx.interfaces;
-}
+//open module com.matteoveroni.routerfx {
+//    requires javafx.base;
+//    requires javafx.graphics;
+//    requires javafx.fxml;
+//    exports com.matteoveroni.routerfx.core;
+//    exports com.matteoveroni.routerfx.dto;
+//    exports com.matteoveroni.routerfx.factory;
+//    exports com.matteoveroni.routerfx.interfaces;
+//}
